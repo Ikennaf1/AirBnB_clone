cce11b59f37df50310b54e4e425cec9c81ee0a6a
Ikennaf1 <iiikbros202@gmail.com>
2023-02-08 23:11:35 +0100

    FileStorage

--- models/base_model.py
+++ models/base_model.py
@@ -30,6 +30,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
         """
@@ -44,6 +45,7 @@ class BaseModel:
         updates the public instance attribute updated_at with current datetime
         """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,13 +6,15 @@ File Storage module
 
 
 import json
+from os import path
+from models.base_model import BaseModel
 
 
 class FileStorage:
     """
     File storage class
     """
-    __file_path = "objects.json"
+    __file_path = "file.json"
     __objects = {}
 
     def all(self):
@@ -26,15 +28,24 @@ class FileStorage:
         sets in `__objects` the obj with key <obj class name>.id
         """
         key = obj.__class__.__name__ + '.' + obj.id
-        self.__object[key] = obj
+        self.__objects[key] = obj
 
     def save(self):
         """
         serializes `__objects` to the JSON file path
         """
-        //
+        json_dict = {}
+        for key, val in self.__objects.items():
+            json_dict[key] = val.to_dict()
+        with open(self.__file_path, mode='w', encoding="utf-8") as f:
+            f.write(json.dumps(json_dict))
 
     def reload(self):
         """
         Deserializes JSON file to `__objects`
         """
+        if path.exists(self.__file_path):
+            with open(self.__file_path, mode='r', encoding="utf-8") as f:
+                json_dict = json.loads(f.read())
+                for key, val in json_dict.items():
+                    self.__objects[key] = eval(val["__class__"])(**val)
