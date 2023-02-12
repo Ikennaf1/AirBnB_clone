4c46c5259692fdb023627cafa7f06d1a069d8907
Ikennaf1 <iiikbros202@gmail.com>
2023-02-12 20:18:38 +0100

    Update Console

--- console.py
+++ console.py
@@ -9,6 +9,12 @@ import cmd
 from datetime import datetime
 import models
 from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 import shlex
 
 
